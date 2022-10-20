c4dac122c87e5e1598706104bf6232ba9ae12f9a
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2022-10-20 19:58:33 +0300

    Socket is connected to the server and data is transmitted to the backend

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {send} from "../socketApi"
 
 function Palette() {
+
+  const [color, setColor] = useState('')
+
   return (
     <div className='palette'>
-        <input type="color" />
-        <button>Click</button>
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)} 
+        />
+      <button onClick={() => send(color)}>Click</button>
     </div>
   )
 }
---
+++ src/socketApi.js
@@ -0,0 +1,16 @@
+import io from "socket.io-client"
+
+let socket
+
+export const init = () => {
+    console.log("Connecting to the server...");
+    socket = io('http://localhost:3001', {
+        transports:["websocket"]
+    })
+
+    socket.on('connect', () => console.log("Connected successfully to the server"))
+}
+
+export const send = (color) => {
+    socket.emit("newColor" ,color)
+}
